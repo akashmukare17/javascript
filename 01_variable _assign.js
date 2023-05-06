5735cc6c06a369bf82b17b2c70c05bfc8772f667
akashmukare17 <akashmukare17@gmail.com>
2023-05-06 23:37:17 +0530

    Variable
    variable declaration and initialization

---
+++ 01_variable _assign.js
@@ -0,0 +1,31 @@
+var myFullName;  //Variable Declaration
+console.log(myFullName);
+myFullName = "Akash Mukare"; //Variable Initialization
+console.log(myFullName);
+myFullName ="Akash Suryakant Mukare" //Variable Update
+console.log(myFullName);
+
+
+var myLove = "I love only JS"; //Variable declaration and Initialization
+console.log(myLove);
+
+var firstName = "Akash"
+console.log(firstName);
+
+var lastName = "Mukare";
+console.log(lastName);
+
+var age ="22";
+console.log(age);
+
+var address = "Barshi, Solapur";
+console.log(address);
+
+var pincode ="413401";
+console.log(pincode);
+
+var gender ="Male";
+console.log(gender);
+
+var isMarried = "No"
+console.log(isMarried);

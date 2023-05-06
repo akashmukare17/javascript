375c7752921c6b80aadb851d934815a5a12ee67a
akashmukare17 <akashmukare17@gmail.com>
2023-05-06 23:21:50 +0530

    Variable

---
+++ hello.js
@@ -0,0 +1,20 @@
+
+
+
+
+console.log("Akash Mukare..");
+
+var firstName= "Akash";
+console.log(firstName);
+
+var age; //Variable Declaration
+age = "22"; //Variable Initialization
+console.log("My age is "+ age);
+
+var holder; // variable declaration
+console.log(holder); //undefined
+holder = "My Spec"; //Variable Initialization
+console.log(holder);
+
+holder = "Sachin spec"; //variable update
+console.log(holder);
